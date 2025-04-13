1c3a4b06368756d6254b0a04b253c452cb34d802
JuanRangel23 <rangelvegajuancamilo@gmail.com>
2025-04-13 09:49:37 -0500

    Update Actividad#1.py

--- Actividad#1.py
+++ Actividad#1.py
@@ -4,22 +4,28 @@
 #Deberas ultilizar super en el metodo de inicializacion (__init_) para reutilizar el codigo de la clase padre (Persona). 
 #Luego crea una instacia de la clase Estudiante e imprime sus atributos y ultiliza sus metodos para asegurarte de que todo funcione correctamente
 
+#Creacion de la clase padre persona
 class Persona:
     def __init__(self, nombre, edad):
         self.nombre = nombre
         self.edad = edad
-    
+
+#creacios de metodo para mostrar informacion
     def MostrarInfo(self):
         print(f"Nombre: {self.nombre}\nEdad: {self.edad} años")
-    
+        
+#creacion de la clase hija Estudiante
 class Estudiante(Persona):
-    def __init__(self, nombre, edad, curso):
+    def __init__(self, nombre, edad, curso): #creacion de atributo de la clase hija
         Persona.__init__(self, nombre, edad)
         self.curso = curso
+
+#creacion de la metodo mostrar informacion que estaheredando de la clase padre
     def MostrarInfo(self):
        super().MostrarInfo()
        print(f"Grado: {self.curso}")
 
+#creacion de objeto
 estudiante = Estudiante("Juan", 25, "5B")
 
 estudiante.MostrarInfo()
