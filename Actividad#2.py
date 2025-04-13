bddde232fd73701b24bf45b7086d016ceff4fa52
JuanRangel23 <rangelvegajuancamilo@gmail.com>
2025-04-13 10:01:05 -0500

    Update Actividad#2.py

--- Actividad#2.py
+++ Actividad#2.py
@@ -4,14 +4,17 @@
 #Deberas ultilizar super en el metodo de inicializacion (__init_) para reutilizar el codigo de la clase padre (Animal). 
 #Luego crea una instacia de la clase Perro e imprime sus atributos y ultiliza sus metodos para asegurarte de que todo funcione correctamente
 
+#creacion de la clase padre Animal
 class Animal:
     def __init__(self, nombre, especie):
         self.nombre = nombre
         self.especie = especie
-    
+ 
+#creacion del metodoa mostrar informacion   
     def MostrarInfo(self):
         print(f"Nombre: {self.nombre}\nEspecie: {self.especie}")
-    
+
+#creacion de la clase hija Perro quien esta heredando de la ce padre los atributos nombre y especie   
 class Perro(Animal):
     def __init__(self, nombre, especie, raza):
         Animal.__init__(self, nombre, especie)
@@ -21,6 +24,7 @@ class Perro(Animal):
        super().MostrarInfo()
        print(f"Raza: {self.raza}")
 
+#crear el objeto mascota
 mascota = Perro("Cannel", "Canino", "Husky Siberiano")
 
 mascota.MostrarInfo()
